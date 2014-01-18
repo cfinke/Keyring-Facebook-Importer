aa60f4ee007680949d93e11d51bf9d92a0fa8966
Christopher Finke <chris@efinke.com>
2014-01-18 04:06:43 +0000

    Fixed typo.

--- keyring-facebook-importer.php
+++ keyring-facebook-importer.php
@@ -8,10 +8,11 @@ Version: 1.0
 Author: Christopher Finke
 Author URI: http://www.chrisfinke.com/
 License: GPL2
+Depends: Keyring, Keyring Social Importers
 */
 
 function keyring_facebook_enable_importer( $importers ) {
-	$importers[] = plugin_dir_path( __FILE__ ) . 'keyring-facebeook-importer/keyring-importer-facebook.php';
+	$importers[] = plugin_dir_path( __FILE__ ) . 'keyring-facebook-importer/keyring-importer-facebook.php';
 	
 	return $importers;
 }
